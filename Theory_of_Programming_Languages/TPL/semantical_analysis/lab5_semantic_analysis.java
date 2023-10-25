59d31b13ff8a1a5d4fbfe2feba8e320db6f60ea6
alea-progaming <mikaela.programming@gmail.com>
2023-10-25 23:31:35 +0800

    rename

--- Theory_of_Programming_Languages/TPL/semantical_analysis/lab5_semantic_analysis.java
+++ Theory_of_Programming_Languages/TPL/semantical_analysis/lab5_semantic_analysis.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class lab5_semantical_analysis {
+public class lab5_semantic_analysis {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
